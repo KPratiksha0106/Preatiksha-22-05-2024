5df1d4c9f13c9e2df6a9a0c507d73ffa304b271e
KPratiksha0106 <pratikshak2000@gmail.com>
2024-06-19 23:28:51 +0530

    first commit

---
+++ com/javaDynamicORRunTimePolymorphism/Test.java
@@ -0,0 +1,19 @@
+package com.javaDynamicORRunTimePolymorphism;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Department department = new Department(121, "");
+		department.getDetails();
+		
+		System.out.println("-----------------------------------------------------------");
+			
+		a employee = new a(132, "Jayesh", "BA", 78393.32);
+		employee.getDetails();
+		
+	}
+
+}
